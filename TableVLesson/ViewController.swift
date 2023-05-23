7009cc6dff701e729cd4331b9aa909fb9a6d3dff
NBonga <bkubheka25@gmail.com>
2023-05-23 15:30:59 +0200

    first commit

--- TableVLesson/ViewController.swift
+++ TableVLesson/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
-
-
 }
 
+//MARK: - Tableview Methods
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as? CustomCell else {return UITableViewCell()}
+        
+        
+        cell.firstNameLabel.text = "John"
+        cell.lastNameLabel.text = "Doe"
+        cell.emailLabel.text = "johndoe@gmail.com"
+        cell.imageLabel.image = UIImage(named: "Iperson.crop.circle.fill")
+        
+        return cell
+    }
+    
+    
+}
